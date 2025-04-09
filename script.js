b8e82b37909dc48aa09acd575b7df518cb1a3592
Obiwumma Pascal <pobiwumma@gmail.com>
2025-04-09 20:27:07 +0100

    Targeted buttons in JS

---
+++ script.js
@@ -0,0 +1,14 @@
+const btn1 = document.getElementById("button1");
+const btn2 = document.getElementById("button2");
+const btn3 = document.getElementById("button3");
+const btn4 = document.getElementById("button4");
+
+const ans1 = document.getElementsByClassName("answer1");
+const ans2 = document.getElementsByClassName("answer2");
+const ans3 = document.getElementsByClassName("answer3");
+const ans4 = document.getElementsByClassName("answer4");
+
+// console.log(ans1[0].innerText)
+// ans1[0].style.display = "none"
+
+btn1
