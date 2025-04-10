9140efd2d483c525c5d4e0e7ae48ce0d4c6f17f2
Obiwumma Pascal <pobiwumma@gmail.com>
2025-04-10 10:13:35 +0100

    Added change in display functionality

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ const btn2 = document.getElementById("button2");
 const btn3 = document.getElementById("button3");
 const btn4 = document.getElementById("button4");
 
+const plus = document.querySelectorAll("#plus");
+const minus = document.querySelectorAll("#minus");
+
 const ans1 = document.getElementsByClassName("answer1");
 const ans2 = document.getElementsByClassName("answer2");
 const ans3 = document.getElementsByClassName("answer3");
@@ -12,14 +15,50 @@ const ans4 = document.getElementsByClassName("answer4");
 // ans1[0].style.display = "none"
 
 btn1.addEventListener("click", () => {
-  ans1[0].style.display = "none"
+  if ( minus[0].style.display === "none" ) {
+    ans1[0].style.display = "none"
+    plus[0].style.display = "none"
+    minus[0].style.display = "block"
+  } else {
+    ans1[0].style.display = "block"
+    plus[0].style.display = "block"
+    minus[0].style.display = "none"
+  }
+
 })
 btn2.addEventListener("click", () => {
-  ans2[0].style.display = "none"
+  if ( minus[1].style.display === "none" ) {
+    ans2[0].style.display = "none"
+    plus[1].style.display = "none"
+    minus[1].style.display = "block"
+  } else {
+    ans2[0].style.display = "block"
+    plus[1].style.display = "block"
+    minus[1].style.display = "none"
+  }
+
 })
 btn3.addEventListener("click", () => {
-  ans3[0].style.display = "none"
+  if ( minus[2].style.display === "none" ) {
+    ans3[0].style.display = "none"
+    plus[2].style.display = "none"
+    minus[2].style.display = "block"
+  } else {
+    ans3[0].style.display = "block"
+    plus[2].style.display = "block"
+    minus[2].style.display = "none"
+  }
+
 })
 btn4.addEventListener("click", () => {
-  ans4[0].style.display = "none"
+  if ( minus[3].style.display === "none" ) {
+    ans4[0].style.display = "none"
+    plus[3].style.display = "none"
+    minus[3].style.display = "block"
+  } else {
+    ans4[0].style.display = "block"
+    plus[3].style.display = "block"
+    minus[3].style.display = "none"
+  }
+
 })
