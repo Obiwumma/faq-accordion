bce8096e444fd7fe44b3d0bd4d8d10146f9ec8e2
Obiwumma Pascal <pobiwumma@gmail.com>
2025-04-10 09:34:50 +0100

    Added minus Icon

--- script.js
+++ script.js
@@ -11,4 +11,15 @@ const ans4 = document.getElementsByClassName("answer4");
 // console.log(ans1[0].innerText)
 // ans1[0].style.display = "none"
 
-btn1
+btn1.addEventListener("click", () => {
+  ans1[0].style.display = "none"
+})
+btn2.addEventListener("click", () => {
+  ans2[0].style.display = "none"
+})
+btn3.addEventListener("click", () => {
+  ans3[0].style.display = "none"
+})
+btn4.addEventListener("click", () => {
+  ans4[0].style.display = "none"
+})
